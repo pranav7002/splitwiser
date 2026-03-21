298d9c4c65d4b0cd114b0674723aefe768c6b285
pranav2007 <raipranav71@gmail.com>
2026-03-22 00:37:25 +0530

    Add models

---
+++ backend/src/models/Expense.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const expenseSchema = new mongoose.Schema(
+  {
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Group",
+      required: true,
+    },
+    paidBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    splits: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        amount: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    description: String,
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Expense", expenseSchema);
